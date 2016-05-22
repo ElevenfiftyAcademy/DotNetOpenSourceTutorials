c2d4f714f03d20561d7781f62f5a06b2a6b75cda
Jordan Willis <jsw0002@gmail.com>
2016-05-22 14:40:22 -0400

    added a foreach challenge

---
+++ ForEachChallenge_JSW/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+BRONZE: 
+	Use the char type to print "Hello World" going vertically down the page.
+
+SILVER: 
+	Do the same thing in a single line.
+	
+GOLD: 
+	Print your name using an array built with chars. It's a type we haven't seen yet. 
+	Use Unicode or hexidecimal numbers to print your name.
+
+	Use the hexadecimal char array with a foreach loop to print your name going down the console. 
+
+char: https://msdn.microsoft.com/en-us/library/x9h8tsay.aspx
+foreach: https://msdn.microsoft.com/en-us/library/ttw7t8t6.aspx
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForEachChallenge_JSW
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			// Bronze
+			foreach (char c in "Hello World!")
+			{
+				Console.WriteLine(c);
+			}
+
+			Console.WriteLine("");
+
+			// Silver
+			foreach (char c in "Hello World!") Console.WriteLine("{0}", c);
+
+			// Gold
+
+			char[] unicodejordan = new char[7];
+
+			unicodejordan[0] = '\u0020'; //adds a space
+			unicodejordan[1] = '\u004a';
+			unicodejordan[2] = '\u006f';
+			unicodejordan[3] = '\u0072';
+			unicodejordan[4] = '\u0064';
+			unicodejordan[5] = '\u0061';
+			unicodejordan[6] = '\u006e';
+
+			foreach (char c in unicodejordan) Console.WriteLine(c);
+
+			char[] hexajordan = new char[7];
+
+			hexajordan[0] = '\x0020'; //adds a space
+			hexajordan[1] = '\x004a';
+			hexajordan[2] = '\x006f';
+			hexajordan[3] = '\x0072';
+			hexajordan[4] = '\x0064';
+			hexajordan[5] = '\x0061';
+			hexajordan[6] = '\x006e';
+
+			foreach (char c in hexajordan) Console.WriteLine(c);
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ ForEachChallenge_JSW/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ForEachChallenge_JSW")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ForEachChallenge_JSW")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("42ac093c-0e8b-4275-8fb6-16c4f8d09d9d")]
